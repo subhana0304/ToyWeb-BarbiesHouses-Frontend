981726a6ffdfccbeb0a0225cd4ac190bf6ccf7b3
Subhana <subhana5721194@gmail.com>
2023-05-19 12:39:18 +0600

    get gallery data from mongodb

--- src/Components/Gellary.jsx
+++ src/Components/Gellary.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 const Gellary = () => {
     const [images, setImages] = useState([]);
     useEffect(()=>{
-        fetch('gellary.json')
+        fetch('http://localhost:5000/gallery')
         .then(res => res.json())
         .then(data => setImages(data))
     },[])
@@ -12,7 +12,7 @@ const Gellary = () => {
             <h1 className='text-5xl font-bold text-[#FF8087] text-center'>Gallery</h1>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  mt-10'>
             {
-                images.map(image => <img src={image.img} alt="" />)
+                images.map(image => <img key={image._id} src={image.img} alt="" />)
             }
         </div>
         </div>
